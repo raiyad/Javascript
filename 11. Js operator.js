8e51b4fb8e8611e9a10822164fdaf43ec9516b8a
Raiyad Alam <52122570+raiyad@users.noreply.github.com>
2021-03-23 00:56:17 +0600

    raiyad

---
+++ 11. Js operator.js
@@ -0,0 +1,50 @@
+//Arithmetic operator
+// + - * / % ++ --
+var a = 10
+var b = 3
+//console.log(a%b)
+
+//Incremental operator
+
+// pre incremental - post incremental
+//console.log(++a)
+//console.log(a++)
+//console.log(a)
+//Assign operator
+//var a = 10
+//var b = 20
+//a+=b
+//console.log(a)
+//a-=b
+//console.log(a)
+//a*=b
+//console.log(a)
+//a/=b
+//console.log(a)
+//a%=b
+//console.log(a)
+
+
+//Comparison operator
+var a = 10
+var b = 10
+console.log(a==b)
+console.log(a!=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+
+var c = 50
+var d = "50"
+
+console.log(c===d)
+console.log(c!==d)
+
+//logical operators
+// &&  ||  !
+//Bitwise operator
+//& | ~ ^ << >>
+
+//Type of operator
+console.log(typeof 10)
---
+++ 12.Js statement.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+var str = 'string'
+var number = 10+(30/5)*5
+
+var a = 10
+var b = 20
+var c = a>=b
+

207deef5a28963329949c0dd55309b5e20cce903
Raiyad Alam <52122570+raiyad@users.noreply.github.com>
2021-04-01 21:23:37 +0600

    raiyad

---
+++ 37.js string length.js
@@ -0,0 +1,13 @@
+var str = 'some string'
+var length = 0
+while (true){
+if(str.charAt(length)==''){
+break
+}
+else{
+    length++
+}
+}
+console.log(length)
+
+console.log(str.length)
---
+++ 38.Array.js
@@ -0,0 +1,3 @@
+//Array
+//Array is a data structure where we can organize, 
+//multiple data under a multiple variable

06aea69c7d06f6dbdd25a4ac90a672eb6a6b536b
Raiyad Alam <52122570+raiyad@users.noreply.github.com>
2021-05-11 02:16:16 +0600

    raiyad

---
+++ 48.Accessing object properties is js.js
@@ -0,0 +1,15 @@
+var point = {
+    x:10,
+    y:20,
+    z:15
+}
+console.log(point.x)
+console.log(point.y)
+console.log(point.x+point.z)
+
+console.log(point['x'])
+console.log(point['z'])
+
+var show = 'x'
+
+console.log(point[show])
---
+++ 50.Remove object properties in js.js
@@ -0,0 +1,22 @@
+var point = {
+    x:10,
+    y:20,
+    z:15
+}
+point.x=45
+point.a =16
+console.log(point)
+console.log(point.d)
+
+point['y']=100
+
+var prop = 'z'
+point[prop] = 55
+
+console.log(point)
+
+point.a= undefined
+console.log(point)
+
+delete point.a 
+console.log(point)
